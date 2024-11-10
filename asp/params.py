0a6a57971f3b6dc1c34d314265b8fac616694f1f
Alexej Lesser <alexej-lesser@lesser-software.com>
2024-11-10 12:59:12 +0100

    Added alexej_based folder and edited Params.py

--- asp/params.py
+++ asp/params.py
@@ -1,3 +1,4 @@
-primary=['asp/graph_based/actions.lp','asp/graph_based/graph.lp','asp/graph_based/traverse.lp']
+#primary=['asp/graph_based/actions.lp','asp/graph_based/graph.lp','asp/graph_based/traverse.lp']
 #primary=['asp/test2.lp']
+primary=['asp/alexej_based/main.lp']
 secondary=[]
