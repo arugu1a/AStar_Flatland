0e445a13816b00d7ecf6f98e6f1c1132a064e119
arugu1a <94646940+arugu1a@users.noreply.github.com>
2024-11-17 13:56:05 +0100

    added shared test env1 and changed encoding to (X,Y)

--- asp/params.py
+++ asp/params.py
@@ -1,4 +1,8 @@
+#examples:
 #primary=['asp/graph_based/actions.lp','asp/graph_based/graph.lp','asp/graph_based/traverse.lp']
 #primary=['asp/test2.lp']
-primary=['asp/alexej_based/main.lp']
+
+primary=['asp/arne_based/main.lp']
+#primary=['asp/alexej_based/main.lp']
+
 secondary=[]
