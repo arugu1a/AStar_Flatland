9df7b4fe2c1480b829c5049fb439fce7dc779abb
Alexej Lesser <alexej-lesser@lesser-software.com>
2024-12-09 10:38:36 +0100

    WIP shared_paths

--- asp/params.py
+++ asp/params.py
@@ -2,10 +2,11 @@
 #primary=['asp/graph_based/actions.lp','asp/graph_based/graph.lp','asp/graph_based/traverse.lp']
 #primary=['asp/test2.lp']
 
-primary=['asp/arne_based/main.lp']
+#primary=['asp/arne_based/main.lp']
 #primary=['asp/alexej_based/main.lp']
-#primary=['asp/shared_paths_based/main.lp']
+primary=['asp/shared_paths_based/main.lp']
 
-secondary=[]
+#secondary=[]
+secondary=['asp/alexej_based/malfunction_handler.lp']
 
 make_GIF = False
